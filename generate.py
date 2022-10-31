f3be24f183e8142061f5fc8be855f1c6f4c1c7f5
Ruben Schmidmeister <4602612+bash@users.noreply.github.com>
2022-10-31 09:09:48 +0100

    Move data to a TOML file

---
+++ generate.py
@@ -0,0 +1,40 @@
+import tomllib
+from html import escape
+
+_LIBRARIES = [
+    'dotnet',
+    'funcky',
+    'fsharp',
+    'rust',
+    'rust_itertools',
+    'python',
+    'more_itertools',
+    'javascript',
+    'npm_itertools',
+    'java',
+]
+
+_ABSENT_OPERATOR = '<td>&mdash;</td>'
+
+_NEWLINE = '\n'
+
+def _operator_cell(operator) -> str:
+    if operator is None:
+        return _ABSENT_OPERATOR
+    else:
+        return f'<td><a href="{escape(operator["url"])}"><code>{operator["name"]}</code></a></td>'
+
+def _operator_row(operator):
+    cells = [_operator_cell(operator.get(library)) for library in _LIBRARIES]
+    return f'<tr>{_NEWLINE.join(cells)}</tr>'
+
+with open("operators.toml", "rb") as f:
+    operators = tomllib.load(f)['operators']
+
+with open("template.html", "r") as f:
+    template = f.read()
+
+rows = _NEWLINE.join((_operator_row(operator) for operator in operators))
+
+with open("generated.html", "w") as f:
+    f.write(template.replace('<!-- OPERATORS -->', rows))
