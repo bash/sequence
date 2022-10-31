7eb64ce39c4b9085b999ce6030a756e8cef09613
Ruben Schmidmeister <4602612+bash@users.noreply.github.com>
2022-10-31 20:53:05 +0100

    Auto-generate links for F#

--- generate.py
+++ generate.py
@@ -18,18 +18,33 @@ _ABSENT_OPERATOR = '<td>&mdash;</td>'
 
 _NEWLINE = '\n'
 
-def _operator_cell(operator) -> str:
+def _operator_cell(operator, library_name: str) -> str:
     if operator is None:
         return _ABSENT_OPERATOR
-    else:
-        url = operator.get('url')
-        label = f'<code>{operator["name"]}</code>'
-        label = f'<a href="{url}">{label}</a>' if url is not None else label
-        return f'<td>{label}</td>'
+
+    operator_name = operator['name']
+    url = operator.get('url') or _default_url(library_name, operator_name)
+    label = f'<code>{escape(operator_name)}</code>'
+    label = f'<a href="{escape(url)}">{label}</a>' if url is not None else label
+    return f'<td>{label}</td>'
+
+
+def _default_url(library_name: str, operator_name: str) -> str:
+    match library_name:
+        case 'fsharp':
+            return _default_fsharp_url(operator_name)
+
+
+def _default_fsharp_url(operator_name: str) -> str:
+    seq_prefix = "Seq."
+    if operator_name.startswith(seq_prefix):
+        return f'https://fsharp.github.io/fsharp-core-docs/reference/fsharp-collections-seqmodule.html#{operator_name[len(seq_prefix):]}'
+
 
 def _operator_row(operator):
-    cells = [_operator_cell(operator.get(library)) for library in _LIBRARIES]
+    cells = [_operator_cell(operator.get(library), library) for library in _LIBRARIES]
     return f'<tr>{_NEWLINE.join(cells)}</tr>'
+
 
 with open("operators.toml", "rb") as f:
     operators = tomllib.load(f)['operators']
